d34b0d9a571e44b256a65d0d3644afce75fa1b14
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-05-17 11:41:57 -0300

    Using props in our components

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,5 +1,5 @@
 //ES module import
-import { Component, h } from "@stencil/core";
+import { Component, h, Prop } from "@stencil/core";
 
 //decorator -> Component -> receives a configuration object
 @Component({
@@ -11,11 +11,19 @@ import { Component, h } from "@stencil/core";
 
 //component -> no need of extends here -> done automatically by stencil.js
 export class SideDrawer {
+  //decorator -> prop -> setting property or listening changes of property
+  @Prop() title: string;
+
   //render method
   render() {
     return (
       <aside>
-        <h1>The Side Drawer</h1>
+        <header>
+          <h1>{this.title}</h1>
+        </header>
+        <main>
+          <slot />
+        </main>
       </aside>
     );
   }
