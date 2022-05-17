e20548712cc16205828a1ebe1d966b808b6ce778
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-05-17 13:18:55 -0300

    Adding a close side drawer button and using mutable props to add functionality to it

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -11,32 +11,25 @@ import { Component, h, Prop } from "@stencil/core";
 
 //component -> no need of extends here -> done automatically by stencil.js
 export class SideDrawer {
-  //decorator -> prop -> setting property or listening changes of property
+  //decorator -> prop -> setting property or listening changes of property (props are inmmutable from inside the component)
   //reflecting prop values to their respective attributes
   @Prop({ reflect: true }) title: string;
-  @Prop({ reflect: true }) open: boolean;
+  //mutable prop
+  @Prop({ reflect: true, mutable: true }) open: boolean;
+
+  //handler
+  onCloseDrawer() {
+    this.open = false; //mutating prop
+    console.log("Closing side drawer...");
+  }
 
   //render method
   render() {
-    //rendering conditionally
-    // let content = null;
-    // if (this.open) {
-    //   content = (
-    //     <aside>
-    //       <header>
-    //         <h1>{this.title}</h1>
-    //       </header>
-    //       <main>
-    //         <slot />
-    //       </main>
-    //     </aside>
-    //   );
-    // }
-    // return content;
     return (
       <aside>
         <header>
           <h1>{this.title}</h1>
+          <button onClick={this.onCloseDrawer.bind(this)}> X </button>
         </header>
         <main>
           <slot />
